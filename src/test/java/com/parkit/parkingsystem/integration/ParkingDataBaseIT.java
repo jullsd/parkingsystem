48c69ea9e595a9a214e13096c6462f35dc9e5213
Julien Sayd <julien.sayd@gmail.com>
2023-01-13 14:18:21 +0100

    #2 fixed of integration tests with TODO comments

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -4,6 +4,9 @@ import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.ParkingSpot;
+import com.parkit.parkingsystem.model.Ticket;
+import com.parkit.parkingsystem.service.FareCalculatorService;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.AfterAll;
@@ -13,6 +16,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Date;
+
+import static java.lang.Thread.sleep;
+import static junit.framework.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -23,11 +32,20 @@ public class ParkingDataBaseIT {
     private static TicketDAO ticketDAO;
     private static DataBasePrepareService dataBasePrepareService;
 
+    private final String VEHICLE_REGLE_NUMBER = "AH707QN";
+
+
+    public String getVEHICLE_REGLE_NUMBER() {
+        return VEHICLE_REGLE_NUMBER;
+
+
+    }
+
     @Mock
     private static InputReaderUtil inputReaderUtil;
 
     @BeforeAll
-    private static void setUp() throws Exception{
+    private static void setUp() throws Exception {
         parkingSpotDAO = new ParkingSpotDAO();
         parkingSpotDAO.dataBaseConfig = dataBaseTestConfig;
         ticketDAO = new TicketDAO();
@@ -38,27 +56,51 @@ public class ParkingDataBaseIT {
     @BeforeEach
     private void setUpPerTest() throws Exception {
         when(inputReaderUtil.readSelection()).thenReturn(1);
-        when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
+        when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn(VEHICLE_REGLE_NUMBER);
         dataBasePrepareService.clearDataBaseEntries();
     }
 
     @AfterAll
-    private static void tearDown(){
+    private static void tearDown() {
 
     }
 
     @Test
-    public void testParkingACar(){
+    public void testParkingACar() {
+
+        //Arrange
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
+        Ticket ticketVehiculeRegleNumberInCommingUser = ticketDAO.getTicket(VEHICLE_REGLE_NUMBER);
+
+        //Act
+        ParkingSpot parkingSpotVehiculeRegleInCommingUser = ticketVehiculeRegleNumberInCommingUser.getParkingSpot();
+        ParkingSpot pakingSpotVehiculeRegleNextInCommingUser = parkingService.getNextParkingNumberIfAvailable();
+
+        // Assert
+        assertThat(ticketVehiculeRegleNumberInCommingUser.getVehicleRegNumber()).isEqualTo(VEHICLE_REGLE_NUMBER);
+        assertThat(parkingSpotVehiculeRegleInCommingUser).isNotSameAs(pakingSpotVehiculeRegleNextInCommingUser);
+
+
         //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
     }
 
     @Test
-    public void testParkingLotExit(){
-        testParkingACar();
+    public void testParkingLotExit() throws InterruptedException {
+        //Arrange
+
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        parkingService.processIncomingVehicle();
+        sleep(1000);
         parkingService.processExitingVehicle();
+        Ticket ticketVehiculeRegleNumberOutComingUser = ticketDAO.getTicket(VEHICLE_REGLE_NUMBER);
+
+
+        //Assert
+        assertThat(ticketVehiculeRegleNumberOutComingUser).isNotNull();
+        assertThat(ticketVehiculeRegleNumberOutComingUser.getOutTime()).isNotNull();
+        assertThat(ticketVehiculeRegleNumberOutComingUser.getPrice()).isNotNull();
+
         //TODO: check that the fare generated and out time are populated correctly in the database
     }
 
